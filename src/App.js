e3fa4c324e10683d8e4cb62150faad79c2b6c761
34680 <116350782+34680@users.noreply.github.com>
2023-02-02 23:04:21 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import {Container} from "react-bootstrap";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Middle from "./components/Middle";
-import Home from "./components/home";
-import Details from "./components/Details";
-import Add from "./components/Add";
+import {Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
+import './index.css';
+import DetailsMovies from "./components/DetailsMovie";
+import AddMovie from "./components/AddMovie";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import './index.css';
 
 function App() {
     return (
         <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/details" element={<Details/>}></Route>
-        <Route path="/add" element={<Add/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/register" element={<Register/>}></Route>
-      </Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/details" element={<DetailsMovies/>}></Route>
+            <Route path="/add" element={<AddMovie/>}></Route>
+            <Route path="/login" element={<Login/>}></Route>
+            <Route path="/register" element={<Register/>}></Route>
+        </Routes>
     );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import Header from "./Header";
-import Middle from "./Middle";
 import Footer from "./Footer";
+import Content from './Content';
 
 function Home() {
     return (
         <>
             <Header></Header>
-            <Middle></Middle>
+            <Content></Content>
             <Footer></Footer>
         </>
     );
